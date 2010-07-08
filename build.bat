beadd7d05398974906c9f72493e0da529035e85a
jon <jon@9114827e-4225-47c5-bf82-cd7e86b77906>
2010-07-08 15:32:45 +0000

    Bea is now its own library
    git-svn-id: http://svn.jonparrott.com/bea@2 9114827e-4225-47c5-bf82-cd7e86b77906

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 mkdir build
 cd build
-cmake -G"Visual Studio 9 2008" -DGLFW_PATH="C:\Users\Jon Wayne\Development\glfw" -DPHOENIX_PATH="C:\Users\Jon Wayne\Development\intrusive" -DBOX2D_PATH="C:\Users\Jon Wayne\Development\Box2D" -DBOOST_ROOT="C:\Program Files\boost\boost_1_42" ..
+cmake -G"Visual Studio 9 2008" -DGLFW_PATH="C:\Users\Jon Wayne\Development\glfw-lite" -DPHOENIX_PATH="C:\Users\Jon Wayne\Development\phoenixgl" -DBOX2D_PATH="C:\Users\Jon Wayne\Development\Box2D" -DBOOST_ROOT="C:\Program Files\boost\boost_1_42" -D_GLFW_WM_MOVE_HACK=TRUE ..
