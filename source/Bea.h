5094998cb5d0b8f1de20b1b110fba6bbee25c571
jon <jon@9114827e-4225-47c5-bf82-cd7e86b77906>
2010-07-08 15:51:47 +0000

    Bea header and CMakeLists.txt fix.
    git-svn-id: http://svn.jonparrott.com/bea@3 9114827e-4225-47c5-bf82-cd7e86b77906

---
+++ source/Bea.h
@@ -0,0 +1,17 @@
+/*
+	Includes all Bea headers.
+*/
+
+#include "AppState.h"
+#include "AppStateManager.h"
+#include "Event.h"
+#include "EventListener.h"
+#include "EventManager.h"
+#include "EventSpy.h"
+#include "Object.h"
+#include "ObjectViewCommon.h"
+#include "PhoenixEvents.h"
+#include "PrintAny.h"
+#include "PropertyContainer.h"
+#include "StringHash.h"
+#include "View.h"
